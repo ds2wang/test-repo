3f6c4468797bec57b65f4b5fcc13b9c258a5c326
David S Wang <dwang@sugarcrm.com>
2014-04-24 11:00:11 -0700

    more fixes

--- simple.js
+++ simple.js
@@ -1,5 +1,14 @@
-/*
+/**
+ * IPC_Test_Config
+ *
+ *
+ * @category Testing
+ * @package  IPC.Test
+ * @author   Rajat Pandit <rajat_pandit@lalaland.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     http://ipcmedia.com
  */
+
 function factorial(n)
 {
     if (n === 0) {
@@ -7,5 +16,5 @@ function factorial(n)
     }
     
     return (n * factorial(n - 1));
-    
+
 }
