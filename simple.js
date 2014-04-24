9addd277f8389078012d043ee69826a0aa2c65cf
David S Wang <dwang@sugarcrm.com>
2014-04-24 11:11:10 -0700

    these standards are annoying

--- simple.js
+++ simple.js
@@ -2,12 +2,12 @@
  * IPC_Test_Config
  *
  * @category  Testing
- * @package  IPC.Test
- * @author  Squiz Pty Ltd <products@squiz.net>
+ * @package    IPC.Test
+ * @subpackage Asdf
+ * @author     Squiz Pty Ltd <products@squiz.net>
  * @license  asdf
  * @link   asdf
- * @subpackage  asdf
- * @copyright  asdf
+ * @copyright  xxxx-xxxx Squiz Pty Ltd (ABN 77 084 670 600)
  */
 
 function factorial(n)
